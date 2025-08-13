ff663b0857a18de93bde0f4b87aa5f63831993bb
di52vum <Salvatore.Cielo@lrz.de>
2025-08-13 16:24:02 +0200

    Major refactor to add a segment timer (and tested it).

--- src/Tmr.hpp
+++ src/Tmr.hpp
@@ -3,23 +3,74 @@
 
 #include <iostream>
 #include <chrono>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+#ifdef MPI
+#include <MPI.h>
+#include <unistd.h>
+#endif
+
 using namespace std;
 using namespace std::chrono;
 
 class Tmr{
+
   private:
-    double zero=0.0, laps=0.0;
-    auto now(){ return duration<double,milli>(steady_clock::now().time_since_epoch()).count()*1.0E-3; }
+    size_t segI = 0  , lapI = 0  ;
+    double tim0 = 0.0, seg0 = 0.0;
+    std::vector<double> segV, lapV;
+    inline auto now(){ return duration<double,milli>(high_resolution_clock::now().time_since_epoch()).count()*1.0E-3; }
+    inline double alg(double xx){ return (!pwsd()) ? (now()-xx) : (-xx); }
+    inline bool pwsd(){ return (tim0<=0.); }
+
   public:
-    void rst(){                  zero = 0.0;              laps = 0.0; }
-    void pws(){ auto v=now(); if(zero>= 0.0) zero+=-v; if(laps>= 0.0) laps+=-v; }
-    // TODO ?? purists may want to put error msg or asserts instead of `return -1. or -2.;` below.
-    // I'd immediately spot a negative time.
-    auto tot(){ auto v=now(); if(zero!= 0.0){ return   (zero>= 0.0)?v-zero:-zero;                     } return -1.; }
-    auto lap(){ auto v=now(); if(laps!= 0.0){ auto x = (laps>= 0.0)?v-laps:-laps; laps = v; return x; } return -2.; }
-    void tik(){ auto v=now(); if(zero<= 0.0) zero+= v; if(laps<= 0.0) laps+= v; }
-    auto tok(){ auto v=tot(); cout<<"Tot time/s: "<<v<<endl; return v; }
-    auto tak(){ auto v=lap(); cout<<"Lap time/s: "<<v<<endl; return v; }
+    Tmr(){ segV.push_back(0.0); lapV.push_back(0.); }
+    inline void tik( ) { if( pwsd()){ tim0+= now(); seg0+= now();}; }
+    inline void pws( ) { if(!pwsd()){ tim0-= now(); seg0-= now();}; }
+    inline void rst( ) { tim0 = 0.0  ; seg0 = 0.0  ; segI=0; } // but keeps segments and laps!
+
+    inline double tok(){return alg(tim0); }
+    inline double seg(){auto xx=alg(seg0)   ; if(!pwsd()){segV[segI++]+=xx; lapV[lapI]+=xx; seg0=now(); if(!lapI)segV.push_back(0.); }; return xx;}
+    inline double lap(){auto xx=lapV[lapI++]; if(!pwsd()){     segI=0;                                           lapV.push_back(0.); }; return xx;}
+    void log(){
+      double mean, max, min; size_t iMax, iMin;
+#ifdef MPI
+#else
+      size_t rank = 0, size=1;
+#endif
+      if(!rank){
+        cout<<"Load imbalance across laps by ranks:"<<std::endl;
+        cout<<"Rank\tmean\t\tiMin: min\t\tmax: iMax"<<endl;
+      }
+      iMax = max_element(lapV.begin(),lapV.end()-1   ) - lapV.begin();
+      iMin = min_element(lapV.begin(),lapV.end()-1   ) - lapV.begin();
+      mean = accumulate (lapV.begin(),lapV.end()-1,.0) /(lapV.size()-1);
+      for(auto i = 0; i < size; ++i)
+       if(!(rank-i)){
+#ifdef MPI
+         usleep(i*300);
+#endif
+         cout <<i<<"\t"<<mean<<"\t"<<iMin<<": "<<lapV[iMin]<<"\t\t"<<iMax<<": "<<lapV[iMax]<<endl;
+       }
+
+      if(!rank){
+        cout<<"Load imbalance across segments by ranks:"<<std::endl;
+        cout<<"Rank\tmean\t\tiMin: min\t\tmax: iMax"<<endl;
+      }
+      iMax = max_element(segV.begin(),segV.end()-1   ) - segV.begin();
+      iMin = min_element(segV.begin(),segV.end()-1   ) - segV.begin();
+      mean = accumulate (segV.begin(),segV.end()-1,.0) /(segV.size()-1);
+      for(auto i = 0; i < size; ++i)
+       if(!(rank-i)){
+#ifdef MPI
+         usleep(i*300);
+#endif
+         cout <<i<<"\t"<<mean<<"\t"<<iMin<<": "<<segV[iMin]<<"\t\t"<<iMax<<": "<<segV[iMax]<<endl;
+       }
+    }
+
 };
 
 #endif
--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,4 @@
+
 #include "../src/Tmr.hpp"  // to test
 #include <unistd.h>   // for sleep
 #include <cassert>    // for ASSERT
@@ -5,14 +6,16 @@
 using namespace std;
 
 template <typename F>
-bool isclose(F value, F reference, F tolerance=5.0E-2){
+bool isclose(F value, F reference, F tolerance=3.0E-3){
   F expression = (reference > tolerance)?abs(reference/value-1.0):abs(reference-value);
   bool outcome = static_cast<bool>(expression <= tolerance);
+#ifndef NDEBUG
   if(!outcome){
     cout<<"They are not close!"<<endl;
     cout<<"  Is value      "<<value     <<" ~= reference "<<reference<<"? If so, raise tolerance!"<<endl;
-    cout<<"  Is expression "<<expression<<" <= tolerance "<<tolerance<<"?"<<endl;
+    cout<<"  Is expression "<<expression<<" ~< tolerance "<<tolerance<<"?"<<endl;
   }
+#endif
   return outcome;
 }
 
@@ -27,79 +30,52 @@ int main() {
     cout<<endl<<"# Testing Tmr"<<endl;
 
     cout<<endl<<"## sleep 1, tok before tik"<<endl;
-    usleep(1E5);
-    assert(isclose(sw.tok(),-1.0));
+    sleep(1);
+    assert(isclose(sw.tok(),0.0));
 
-    cout<<endl<<"## sleep 100msec, tak (=print lap) before tik"<<endl;
-    usleep(1E5);
-    assert(isclose(sw.tak(), -2.0));
-
-    cout<<endl<<"## Reset, sleep 100msec, tot  "<<endl;
+    cout<<endl<<"## Reset, sleep 100msec, tok  "<<endl;
     sw.rst(); sw.tik(); usleep(1E5);
-    assert(isclose(sw.tot(), 1.0E-1));
+    assert(isclose(sw.tok(), 1.0E-1));
 
     cout<<endl<<"## tik while already running, sleep 100msec, tok "<<endl; 
     sw.tik(); usleep(1E5);
     assert(isclose(sw.tok(), 2.0E-1));
 
-    cout<<endl<<"## tik while already running, sleep 100msec, tot "<<endl; 
+    cout<<endl<<"## tik while already running, sleep 100msec, tok "<<endl; 
     sw.tik(); usleep(1E5);
-    assert(isclose(sw.tot(), 3.0E-1));
+    assert(isclose(sw.tok(), 3.0E-1));
 
-    cout<<endl<<"## pws (pause), sleep 100msec, tot "<<endl;
-    sw.pws(); usleep(1E5); auto tmp = sw.tot(); // save it for later
+    cout<<endl<<"## pws (pause), sleep 200msec, tok "<<endl;
+    sw.pws(); usleep(2E5); auto tmp = sw.tok();
     assert(isclose(tmp, 3.0E-1));
 
-    cout<<endl<<"## pws while already paused, sleep 100msec, tot "<<endl; 
+    cout<<endl<<"## pws while already paused, sleep 100msec, tok "<<endl; 
     sw.pws(); usleep(1E5);
-    assert(isclose(sw.tot(), tmp, 0.0)); //exact as above
+    assert(isclose(sw.tok(), tmp, 0.0)); //exact as above
 
-    cout<<endl<<"## tik from paused, sleep 100msec, tot"<<endl;     
+    cout<<endl<<"## tik from paused, sleep 100msec, tok"<<endl;
     sw.tik(); usleep(1E5);
-    assert( isclose(sw.tot(), 4.0E-1));
+    assert( isclose(sw.tok(), 4.0E-1));
 
-    cout<<endl<<"## print first lap, should be same as tot "<<endl;  
-    assert( isclose(sw.lap(), 4.0E-1));
+    cout<<endl<<"## tik again, sleep 300msec, tok"<<endl;
+    sw.tik(); usleep(3E5);
+    assert( isclose(sw.tok(), 7.0E-1));
 
-    cout<<endl<<"## sleep 200msec, print next lap "<<endl;     
-    usleep(2.0E5);
-    assert( isclose(sw.lap(), 2.0E-1));
+    sw.rst(); sw.tik();
 
-    cout<<endl<<"## pws, sleep 200msec, print next lap "<<endl; 
-    sw.pws(); usleep(2.0E5);
-    assert( isclose(sw.lap(), 0.0));
 
-    cout<<endl<<"## restart with tik, sleep 120msec, tak "<<endl; 
-    sw.tik(); usleep(1.2E5);
-    assert( isclose(sw.tak(), 1.2E-1));
-
-    cout<<endl<<"## usleep 120msec again, tak again "<<endl; 
-    usleep(1.2E5);
-    assert( isclose(sw.tak(), 1.2E-1));
-
-    cout<<endl<<"## just tok "<<endl;
-    assert( isclose(sw.tok(), 8.4E-1));
-
-    cout<<endl<<"## a second tok "<<endl;
-    assert( isclose(sw.tok(), 8.4E-1));
-
-    cout<<endl<<"## 4x tak "<<endl;
-    assert( isclose(sw.tak(),0.0));
-    assert( isclose(sw.tak(),0.0));
-    assert( isclose(sw.tak(),0.0));
-    assert( isclose(sw.tak(),0.0));
-
-    cout<<endl<<"## reset, NO tik, then tok "<<endl; 
-    sw.rst();
-    assert( isclose(sw.tok(),-1.0));
-
-    cout<<endl<<"## reset,    tik, then tok "<<endl; 
-    sw.rst();   sw.tik();
-    assert( isclose(sw.tok(),0.0));
-
-    cout<<endl<<"## sleep 200msec, tok "<<endl;
-    usleep(2.0E5);
-    assert( isclose(sw.tok(),2.0E-1));
+    double dt, sum;
+    for(auto step=0; step<3; (++step) ){
+      sum=0;
+      for(auto seg=0; seg<4; ++seg){
+        cout<<endl<<"## step "<<step<<" seg "<<seg;
+        dt = seg/10.; usleep(dt*1.0E6); sum+=dt;
+        assert(isclose(sw.seg(), dt) );
+      }
+      assert(isclose(sw.lap(),sum) );
+    }
+    cout<<endl;
+    sw.log();
 
     cout<<endl<<"# All tests passed! :D "<<endl;
 
